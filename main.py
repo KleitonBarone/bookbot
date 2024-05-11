e67e16ff2c19b59fe2e3e3e35d85c1aea9c23c9e
KleitonBarone <25616578+KleitonBarone@users.noreply.github.com>
2024-05-10 21:18:17 -0300

    feat: add get_book_text function

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hello world")
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def main() :
+    path_to_file = "books/frankenstein.txt"
+    file_contents = get_book_text(path_to_file)
+    print(file_contents)
+
+main()
