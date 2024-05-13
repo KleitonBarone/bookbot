4e4b8247535bd0f5594239f42a5493b3fd586ca2
KleitonBarone <25616578+KleitonBarone@users.noreply.github.com>
2024-05-13 20:18:38 -0300

    feat: change count_letter_occurrences, add dictionary_to_list_of_dictionaries, add sort_on_value and print_report

--- main.py
+++ main.py
@@ -11,18 +11,36 @@ def count_letter_occurrences(text):
     letters = {}
     for word in text.split():
         for letter in word:
+            if not letter.isalpha():
+                continue
             if letter in letters:
                 letters[letter] += 1
             else:
                 letters[letter] = 1
     return letters
 
+def dictionary_to_list_of_dictionaries(dictionary):
+    return [{"key": key, "value": value} for key, value in dictionary.items()]
+
+def sort_on_value(dictionary):
+    return dictionary["value"]
+
+def print_report(path_to_file, word_count, letter_count_list):
+    print(f"--- Begin report of {path_to_file} ---")
+    print(f"{word_count} words found in the document")
+    print("")
+    for letter in letter_count_list:
+        print(f"The {letter['key']} character was found {letter['value']} times")
+    print("--- End report ---")
+
 def main():
     path_to_file = "books/frankenstein.txt"
     file_contents = get_book_text(path_to_file)
     word_count = get_word_count(file_contents)
     letter_count = count_letter_occurrences(file_contents)
-    print(word_count)
-    print(letter_count)
+    letter_count_list = dictionary_to_list_of_dictionaries(letter_count)
+    letter_count_list.sort(key=sort_on_value, reverse=True)
+    print_report(path_to_file, word_count, letter_count_list)
+
 
 main()
